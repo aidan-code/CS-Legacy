ae1219b86e6a0619a332b5f735aff76a6c016033
Adegbamigbe Opeyemi <fiyinadegbamigbe@gmail.com>
2022-05-30 01:31:44 +0100

    New Updates
    Updates on the landing page
    , the FAQ page and the style sheet

--- func.js
+++ func.js
@@ -1,3 +1,29 @@
+const arrow = document.querySelectorAll(".push");
+const dTxt = document.querySelectorAll(".drop");
+const dashed = document.querySelectorAll(".barr");
+console.log(dTxt, arrow, dashed);
+
+arrow.forEach(item => {
+  item.addEventListener('click', () =>{
+    dTxt.classList.toggle('visib');
+    dashed.classList.toggle('visib');
+    arrow.classList.toggle('rota');
+  })
+});
+
+
+// arrow.addEventListener("click", function onclick () {
+//     dTxt.classList.toggle('visib');
+//     dashed.classList.toggle('visib');
+//     arrow.classList.toggle('rota');
+
+//   });
+
+
+
+
+
+
 const bars = document.querySelector('.menu');
 const navUl = document.querySelector('.hd-nav');
 // the bars
@@ -7,6 +33,9 @@ const thrdBar = document.querySelector(".bar3");
 
 console.log(bars);
 
+
+
+
 bars.addEventListener("click", () =>{
     navUl.classList.toggle('show')
     firstBar.classList.toggle('right');
@@ -14,3 +43,8 @@ bars.addEventListener("click", () =>{
   thrdBar.classList.toggle('trnleft');
     // console.log(nav
 });
+
+
+
+
+
